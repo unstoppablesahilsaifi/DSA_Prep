f0e9c9beec68f6de17641db0a9a9790f0ca1f9c3
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-07-14 15:37:58 +0530

    Update Time_Space_Complexity.java

--- Time_Space_Complexity.java
+++ Time_Space_Complexity.java
@@ -1,1 +1,10 @@
-//Test
+// From Anuj Bhaiyya Youtube Channel
+/* Time Complexity- Time taken by an algorithm as a function of the length of input.
+a. f(n)=3n^2+5n -> Ingnore constants and focus on the maximum power then time complexity would be-> Big O(n^2)
+b. f(n)=n+100log n -> n is greater beacuse if we make the graph of this function log n will always below the n it would be downgrading graph so TC would be Big O(n).
+c. f(n)=1000000 -> for this type of function TC would be O(1) coz 100000 is a constant value.
+Some other examples for O(n)=n/4, (n+100)/4, 5n+2log n
+Some other examples for O(n^2)=5n^2+7n+2, 3n^2+2n, n^2/100
+Note- Big O notation works on worst case because that vo case hi sabse jyada time lega,to idea ye hota h ki minimum kitna bhi time le algo but atmost(worst case) itna to
+time legi hi legi.
++++For practice got to interview bit+++
